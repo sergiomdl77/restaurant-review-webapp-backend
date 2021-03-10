a5e53b16275247304d11fbb5cfadfc6d646d0573
sergiomdl77 <sergiomdl77@gmail.com>
2021-03-10 11:30:19 -0500

    First commit (Creation of blank project to build upon).

--- src/main/java/com/delgadotw/myfirstwebapp/MyFirstWebappApplication.java
+++ src/main/java/com/delgadotw/myfirstwebapp/MyFirstWebappApplication.java
@@ -2,7 +2,7 @@ package com.delgadotw.myfirstwebapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+// Entry point
 @SpringBootApplication
 public class MyFirstWebappApplication {
 
