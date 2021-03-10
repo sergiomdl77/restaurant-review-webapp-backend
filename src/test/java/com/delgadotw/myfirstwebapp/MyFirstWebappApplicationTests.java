01c1be192f5a5d16341f05af6d7c55fe995c90c2
sergiomdl77 <sergiomdl77@gmail.com>
2021-03-10 11:22:41 -0500

    First commit (Creation of blank project to build upon).

---
+++ src/test/java/com/delgadotw/myfirstwebapp/MyFirstWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.delgadotw.myfirstwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFirstWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
