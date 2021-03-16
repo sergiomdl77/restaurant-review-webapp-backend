52f643984ba5167565b1d3e19a930e61a0e835fc
sergiomdl77 <sergiomdl77@gmail.com>
2021-03-15 21:56:44 -0400

    First attempt to connect to postgres database from Heroku add-ons.

--- src/main/java/com/delgadotw/myfirstwebapp/service/RestaurantService.java
+++ src/main/java/com/delgadotw/myfirstwebapp/service/RestaurantService.java
@@ -2,10 +2,12 @@ package com.delgadotw.myfirstwebapp.service;
 
 import com.delgadotw.myfirstwebapp.data.RestaurantRepository;
 import com.delgadotw.myfirstwebapp.models.Restaurant;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class RestaurantService
 {
     private final RestaurantRepository restaurantRepository;
