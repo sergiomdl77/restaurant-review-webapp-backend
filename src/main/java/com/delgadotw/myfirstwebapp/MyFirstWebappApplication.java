2181669518351d535867c05e6448bd885dc417f5
sergiomdl77 <sergiomdl77@gmail.com>
2021-03-23 22:57:06 -0400

    CorsFilter Bean added for configuration to develop in locally.

--- src/main/java/com/delgadotw/myfirstwebapp/MyFirstWebappApplication.java
+++ src/main/java/com/delgadotw/myfirstwebapp/MyFirstWebappApplication.java
@@ -2,6 +2,13 @@ package com.delgadotw.myfirstwebapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
+
 // Entry point
 @SpringBootApplication
 public class MyFirstWebappApplication {
@@ -10,4 +17,24 @@ public class MyFirstWebappApplication {
 		SpringApplication.run(MyFirstWebappApplication.class, args);
 	}
 
+	// This Cors Filter is a bean for configuration that will tell this backend application (which is running in one
+	// domain/origin) to allow the frontend application (which is running in a different domain/origin) to access
+	// resources on this backend (which happens when we are running both frontend and backend locally for development).
+	@Bean
+	public CorsFilter corsFilter() {
+		CorsConfiguration corsConfiguration = new CorsConfiguration();
+		corsConfiguration.setAllowCredentials(true);
+		corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
+				"Accept", "Authorization", "Origin", "Accept", "X-Requested-With",
+				"Access-Control-Request-Method", "Access-Control-Request-Headers"));
+		corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Authorization",
+				"Access-Control-Allow-Origin", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));
+		corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+		UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+		urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+		return new CorsFilter(urlBasedCorsConfigurationSource);
+
+	}
 }
+
