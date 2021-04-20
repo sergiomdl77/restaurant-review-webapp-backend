a0511ccbab214bd46dc2a87f017fd1896cabe77d
sergiomdl77 <sergiomdl77@gmail.com>
2021-04-20 14:33:05 -0400

    Changed the type of the date fields in Restaurant and Review models to string type so they can be persisted more easily.

--- src/main/java/com/delgadotw/myfirstwebapp/MyFirstWebappApplication.java
+++ src/main/java/com/delgadotw/myfirstwebapp/MyFirstWebappApplication.java
@@ -25,7 +25,8 @@ public class MyFirstWebappApplication {
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.setAllowCredentials(true);
 	//	corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));   // for development
-		corsConfiguration.setAllowedOrigins(Arrays.asList("https://restaurant-review-webapp-f-end.herokuapp.com"));   // for production
+		corsConfiguration.setAllowedOrigins(Arrays.asList("https://restaurant-review-webapp-f-end.herokuapp.com",
+				"http://restaurant-review-webapp-f-end.herokuapp.com"));   // for production));   // for production
 
 		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
 				"Accept", "Authorization", "Origin", "Accept", "X-Requested-With",
